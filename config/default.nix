3122c2182133ca1ba2969af720b0bd0d74c22161
Matt Sturgeon <matt@sturgeon.me.uk>
2023-09-14 19:15:12 +0100

    Initial commit

---
+++ config/default.nix
@@ -0,0 +1,10 @@
+{ pkgs ?  import <nixpkgs> {} }:
+
+let
+  firmware = import ../src {};
+  config = ./.;
+
+  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+
+in firmware.combine_uf2 glove80_left glove80_right
