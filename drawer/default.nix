e5b3136cd12f61211872ea13dafcf0e511e56b40
Matt Sturgeon <matt@sturgeon.me.uk>
2024-03-24 19:14:01 +0000

    Add keymap-drawer-configs package

--- drawer/default.nix
+++ drawer/default.nix
@@ -5,11 +5,14 @@
 }: let
 in {
     perSystem = {
+      config,
       pkgs,
       system,
       ...
     }: let
       inherit (inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+      pkg = config.packages.keymap-drawer;
+      exe = lib.getExe pkg;
     in {
       packages = {
         keymap-drawer = mkPoetryApplication {
@@ -20,5 +23,24 @@ in {
             homepage = "https://github.com/caksoylar/keymap-drawer";
           };
         };
+
+        keymap-drawer-configs = pkgs.runCommand "configs" {} ''
+          echo "writing config to $out"
+          mkdir -p $out
+
+          # TODO set generic vars in one place globally
+          CONFIG="${../config/keymap_drawer.yaml}"
+
+          # TODO consider running through `parallel`?
+          for KEYMAP in ${ ../config }/*.keymap; do
+            KEYBOARD=$(basename -s .keymap "$KEYMAP")
+            OUTPUT="$out"/"$KEYBOARD".yaml
+
+            echo "Parsing keymap for $KEYBOARD"
+            # ${exe} -c "$CONFIG" parse -z "$KEYMAP" > "$OUTPUT"
+            ${exe} parse -z "$KEYMAP" > "$OUTPUT"
+          done
+        '';
+      };
     };
 }
