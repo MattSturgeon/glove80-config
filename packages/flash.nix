7a10c2b664845bd5c81748c31b766145e3916db4
Matt Sturgeon <matt@sturgeon.me.uk>
2024-04-03 23:47:07 +0100

    feat(flash): Ask to run again

--- packages/flash.nix
+++ packages/flash.nix
@@ -29,74 +29,94 @@ writeShellApplication {
       sed -e 's/^/    /'
     }
 
-    # Platform specific disk candidates
-    declare -a disks
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-      # Linux/GNU
-      # - /run/media/<user>/<disk>
-      disks+=(
-        /run/media/"$(whoami)"/GLV80*
-        /media/GLV80*
-        /mnt/*
-      )
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-      # Mac OSX
-      # - /Volumes/<disk>
-      disks=(/Volumes/GLV80*)
-    elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
-      # Cygwin or Msys2
-      # - /<drive letter>
-      disks=(/?)
-    elif (grep -sq Microsoft /proc/version); then
-      # WSL
-      # - /mnt/<drive letter>
-      disks=(/mnt/?)
-    else
-      echo "Error: Unable to detect platform!"
-      echo "OSTYPE=$OSTYPE"
-      echo "/proc/version"
-      indent < /proc/version
-      exit 1
-    fi
-
-    # Disks that have a matching INFO_UF2
-    declare -a matches
-    for disk in "''${disks[@]}"; do
-      if (grep -sq Glove80 "$disk"/INFO_UF2.TXT); then
-        matches+=("$disk")
+    # Prints a list of connected glove80s
+    list_keyboards() {
+      # Platform specific disk candidates
+      declare -a disks
+      if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux/GNU
+        # - /run/media/<user>/<disk>
+        disks+=(
+          /run/media/"$(whoami)"/GLV80*
+          /media/GLV80*
+          /mnt/*
+        )
+      elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        # - /Volumes/<disk>
+        disks=(/Volumes/GLV80*)
+      elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+        # Cygwin or Msys2
+        # - /<drive letter>
+        disks=(/?)
+      elif grep -sq Microsoft /proc/version; then
+        # WSL
+        # - /mnt/<drive letter>
+        disks=(/mnt/?)
+      else
+        echo "Error: Unable to detect platform!" >&2
+        echo "OSTYPE=$OSTYPE" >&2
+        echo "/proc/version" >&2
+        indent < /proc/version >&2
       fi
-    done
 
-    # Assert we found exactly one keyboard
-    count="''${#matches[@]}"
-    if (( count < 1 )); then
-      # No matches. Exit
-      echo "Error: No Glove80 connected!"
-      exit 1
-    elif (( count > 1 )); then
-      # Multiple matches. Print them and exit
-      echo "Error: $count Glove80s connected. Expected 1!"
-      for i in "''${!matches[@]}"; do
-        kb="''${matches[$i]}"
-        # Print the relevant lines from INFO_UF2
-        echo "$((i + 1)). $kb"
-        grep --no-filename --color=never Glove80 "$kb"/INFO_UF2.TXT | indent
+      # Print disks that have a matching INFO_UF2
+      for disk in "''${disks[@]}"; do
+        if (grep -sq Glove80 "$disk"/INFO_UF2.TXT); then
+          echo "$disk"
+        fi
       done
-      exit 1
-    fi
+    }
 
-    # We have a winner!
-    kb="''${matches[0]}"
-    echo "Found keyboard:"
-    echo "$kb"
-    indent < "$kb"/INFO_UF2.TXT
-    echo
+    flash() {
+      declare -a matches
+      readarray -t matches < <(list_keyboards)
 
-    # Flash by copying the firmware package
-    if gum spin --title "Flashing firmware..." \
-      -- cp -Tfr ${firmware} "$kb"
-    then echo "Firmaware flashed!"
-    else echo "Error flashing firmware!"
-    fi
+      # Assert we found exactly one keyboard
+      count="''${#matches[@]}"
+      if (( count < 1 )); then
+        # No matches. Exit
+        echo "Error: No Glove80 connected!"
+        return 1
+      elif (( count > 1 )); then
+        # Multiple matches. Print them and exit
+        echo "Error: $count Glove80s connected. Expected 1!"
+        for i in "''${!matches[@]}"; do
+          kb="''${matches[$i]}"
+          # Print the relevant lines from INFO_UF2
+          echo "$((i + 1)). $kb"
+          grep --no-filename --color=never Glove80 "$kb"/INFO_UF2.TXT | indent
+        done
+        return 1
+      fi
+
+      # We have a winner!
+      kb="''${matches[0]}"
+      echo "Found keyboard:"
+      echo "$kb"
+      indent < "$kb"/INFO_UF2.TXT
+      echo
+
+      # Flash by copying the firmware package
+      if gum spin --title "Flashing firmware..." \
+        -- cp -Tfr ${firmware} "$kb"
+      then echo "Firmaware flashed!"
+      else echo "Error flashing firmware!"
+      fi
+    }
+
+    # Run the script
+    flash
+    while
+      gum confirm \
+        --default="no" \
+        --affirmative="Run again" \
+        --negative="Quit" \
+        "Flash another keyboard?"
+    do
+      echo # blankline
+      echo "Running again"
+      flash
+    done
   '';
 }
