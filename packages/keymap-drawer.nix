f45a32f3774b1dfb266b26f1220d638375f4a44e
Matt Sturgeon <matt@sturgeon.me.uk>
2025-06-16 05:26:41 +0100

    fix: keymap-drawer package

--- packages/keymap-drawer.nix
+++ packages/keymap-drawer.nix
@@ -1,10 +1,39 @@
-{inputs, ...}: {
-  perSystem = {pkgs, ...}: let
-    poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
-  in {
-    packages.keymap-drawer = poetry2nix.mkPoetryApplication {
-      projectDir = inputs.keymap-drawer;
-      preferWheels = true;
+{lib, inputs, ...}: {
+  perSystem = {pkgs, ...}: {
+    packages.keymap-drawer = let
+      src = inputs.keymap-drawer;
+      pyproject = lib.importTOML "${src}/pyproject.toml";
+    # TODO: upstream to nixpkgs
+    in pkgs.python3Packages.buildPythonApplication {
+      pname = pyproject.project.name;
+      inherit (pyproject.project) version;
+      inherit src;
+
+      pyproject = true;
+      build-system = with pkgs.python3Packages; [
+        poetry-core
+      ];
+
+      dependencies = with pkgs.python3Packages; [
+        pcpp
+        platformdirs
+        pydantic
+        pydantic-settings
+        pyparsing
+        pyyaml
+        tree-sitter
+        tree-sitter-grammars.tree-sitter-devicetree
+      ];
+
+      doCheck = true;
+      pythonImportsCheck = [ "keymap_drawer" ];
+
+      doInstallCheck = true;
+      nativeInstallCheckInputs = [
+        pkgs.versionCheckHook
+      ];
+      versionCheckProgramArg = "--version";
+
       meta = {
         mainProgram = "keymap";
         homepage = "https://github.com/caksoylar/keymap-drawer";
